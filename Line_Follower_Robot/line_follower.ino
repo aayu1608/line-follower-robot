f425b7dccb3f82edc57e67a9acd5c94ef8d1dd97
Aayushi <actidke@gmail.com>
2026-06-20 23:19:49 +0530

    feat: arduino code

---
+++ Line_Follower_Robot/line_follower.ino
@@ -0,0 +1,104 @@
+
+#define IR_SENSOR_RIGHT 12
+#define IR_SENSOR_LEFT 11
+#define MOTOR_SPEED 140
+
+// Right motor
+int enableRightMotor = 6;
+int rightMotorPin1 = 7;
+int rightMotorPin2 = 8;
+
+// Left motor
+int enableLeftMotor = 5;
+int leftMotorPin1 = 9;
+int leftMotorPin2 = 10;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  TCCR0B = TCCR0B & B11111000 | B00000010;
+
+  pinMode(enableRightMotor, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
+
+  pinMode(enableLeftMotor, OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+
+  pinMode(IR_SENSOR_RIGHT, INPUT);
+  pinMode(IR_SENSOR_LEFT, INPUT);
+
+  rotateMotor(0, 0);
+}
+
+void loop()
+{
+  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+
+  Serial.print("L=");
+  Serial.print(leftIRSensorValue);
+  Serial.print(" R=");
+  Serial.println(rightIRSensorValue);
+
+  // White surface
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+  }
+  // Right sensor on line
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW)
+  {
+    rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
+  }
+  // Left sensor on line
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH)
+  {
+    rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);
+  }
+  // Both sensors on line
+  else
+  {
+    rotateMotor(0, 0);
+  }
+}
+
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+{
+  if (rightMotorSpeed < 0)
+  {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
+  }
+  else if (rightMotorSpeed > 0)
+  {
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
+  }
+  else
+  {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, LOW);
+  }
+
+  if (leftMotorSpeed < 0)
+  {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+  }
+  else if (leftMotorSpeed > 0)
+  {
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
+  }
+  else
+  {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, LOW);
+  }
+
+  analogWrite(enableRightMotor, abs(rightMotorSpeed));
+  analogWrite(enableLeftMotor, abs(leftMotorSpeed));
+}
